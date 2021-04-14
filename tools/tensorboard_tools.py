8d55a3bf5de2503332865dd7bfeb541b7d4a725c
Szymon Kulpinski <skulpinski@student.ethz.ch>
2021-04-14 11:11:10 +0200

    Copy tensorboard_tools.py from subpixel

--- tools/tensorboard_tools.py
+++ tools/tensorboard_tools.py
@@ -1,49 +1,37 @@
 import torchvision
-import torch
 import numpy as np
 import torch
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib import pyplot as pl;
-
 pl.ion()
 from scipy.ndimage import uniform_filter
 smooth = lambda arr: uniform_filter(arr, 3)
-
-
 
 def add_images_tensorboard(pred, writer, model_name, epoch):
     if model_name == 'r2d2':
         for key in pred.keys():
             if key != 'descriptors':
-                img_grid = torchvision.utils.make_grid(pred[key][0])
-                img_grid2 = torchvision.utils.make_grid(pred[key][1])
+                img_grid = torchvision.utils.make_grid(pred[key][0], normalize=True)
+                img_grid2 = torchvision.utils.make_grid(pred[key][1], normalize=True)
                 combined_grid = torch.cat((img_grid, img_grid2), 1)
                 writer.add_image(key, combined_grid, epoch)
 
+                # img0 = img_grid.cpu().numpy().transpose(1, 2, 0)
+                # img1 = not_norm.cpu().numpy().transpose(1, 2, 0)
+                # print("sth")
+
         new_pred = get_one_batch(pred)
         writer.add_image("visual", get_image_without_keypoints(new_pred), epoch)
-
-        # test = try_sth(new_pred)
-        # writer.add_image("visual", get_image_without_keypoints(new_pred), epoch)
         writer.close()
-
     return
 
 def get_one_batch(pred):
     new_pred ={}
     for key in pred.keys():
         new_pred[key] = pred[key][0][0:1] # so that it still keeps dim 1 of the tensor and not only image
+    new_pred['imgs'] = torchvision.utils.make_grid(new_pred['imgs'], normalize=True)
     return new_pred
-
-def try_sth(pred): #CLEAN: clean me if not using
-    img0 = pred['imgs']
-    img = img0.cpu().numpy()
-    img = img[0].transpose(1, 2, 0)
-    rela = pred.get('reliability')[0].cpu().detach().numpy().transpose(1, 2, 0)
-    repe = pred.get('repeatability')[0].cpu().detach().numpy().transpose(1, 2, 0)
-    # backtorgb = cv2.cvtColor(rela, cv2.COLOR_GRAY2RGB)
-    pass
 
 def transparent(img, alpha, cmap, **kw):
     from matplotlib.colors import Normalize
@@ -56,7 +44,7 @@ def get_image_without_keypoints(pred):
 
     img0 = pred['imgs']
     img = img0.cpu().numpy()
-    img = img[0].transpose(1,2,0)
+    img = img.transpose(1,2,0) # passed already as 3 dim tensor, not 4
 
     with torch.no_grad():
         res = pred
